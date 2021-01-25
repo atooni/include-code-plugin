f57edfc02058b0ad1ea09be3d1b99c067a2edad1
Andreas Gies <andreas@wayofquality.de>
2021-01-25 14:53:41 +0100

    Add hendling of local files and rudimentary tests

--- src/__tests__/fetchCode.js
+++ src/__tests__/fetchCode.js
@@ -1,8 +1,16 @@
-import { fetchCodeFromUrl } from '../fetchCode';
+import { fetchCodeFromFile, fetchCodeFromUrl } from '../fetchCode';
 
 test('Should succeed with a valid code url', async () => {
   const actual = await fetchCodeFromUrl("https://raw.githubusercontent.com/blended-zio/blended-zio/aa08573871c84dfddab3af88991ec01ed58c19bf/blended.zio.jmx/src/main/scala/blended/zio/jmx/JmxAttribute.scala")
-  //const actual = await fetchCodeFromUrl("http://raw.doesnotexist.de/mycode.scala")
-  console.log(actual)
   expect(actual.startsWith("package")).toBe(true)
+})
+
+test('Should succeed getting a file from the file system (relative)', () => {
+  const actual = fetchCodeFromFile("src/transform.ts")
+  expect(actual.startsWith("import visit")).toBe(true)
+})
+
+test('Should succeed getting a file from the file system (absolute)', () => {
+  const actual = fetchCodeFromFile(process.cwd() + "/src/transform.ts")
+  expect(actual.startsWith("import visit")).toBe(true)
 })
--- src/fetchCode.ts
+++ src/fetchCode.ts
@@ -1,7 +1,15 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
 
 export const fetchCodeFromUrl = async (url: string) => {
   const response = await fetch(url);
   const content: string = await response.text();
   return content;
 };
+
+export const fetchCodeFromFile = (fileName: string) => {
+  let name = fileName
+  if (!fileName.startsWith("/")) name = process.cwd() + "/" + name
+  return fs.readFileSync(name, "utf-8")
+}
